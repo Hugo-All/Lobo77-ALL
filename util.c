e228afe0307d55c32fa09116d8be83be27f63db6
RedBlaze42 <allairehugo@gmail.com>
2021-04-10 00:06:34 +0200

    Ajout fonction couleur

---
+++ util.c
@@ -0,0 +1,7 @@
+#include "util.h"
+
+void color (int couleurDuTexte, int couleurDuFond)
+{
+     HANDLE H = GetStdHandle(STD_OUTPUT_HANDLE);
+     SetConsoleTextAttribute(H, couleurDuFond*16+couleurDuTexte);
+}
--- util.h
+++ util.h
@@ -1,1 +1,22 @@
+#include <windows.h>
 
+enum E_couleurs{noir,
+            bleu_fonce,
+            vert_fonce,
+            turquoise,
+            rouge_fonce,
+            violet,
+            vert_oie,
+            gris_clair,
+            gris_fonce,
+            bleu,
+            vert,
+            turquoise_2,
+            rouge,
+            violet_2,
+            jaune,
+            blanc
+            };
+typedef enum E_couleurs couleurs;
+
+void color (int couleurDuTexte, int couleurDuFond);
