919466e34497cd00422046dbd15f99974f12c854
RedBlaze42 <allairehugo@gmail.com>
2021-04-10 12:09:17 +0200

    Fonction bannière

--- affichage.c
+++ affichage.c
@@ -1,1 +1,12 @@
 #include "affichage.h"
+
+void banniere()
+{
+    printf("%c",200);
+    for(int i; i<15; i++)
+        printf("%c",205);
+    printf("LOBO 77");
+    for(int i; i<15; i++)
+        printf("%c",205);
+    printf("%c",188);
+}
