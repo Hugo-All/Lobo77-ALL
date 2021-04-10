543c768f453cad31e906ac4bc586619a767fc416
RedBlaze42 <allairehugo@gmail.com>
2021-04-10 12:22:40 +0200

    Typo

--- types.h
+++ types.h
@@ -1,7 +1,7 @@
 #define DIM_STR 10
 #define DIM_pile 56
 #define DIM_main_joeur 5
-#define NB_max_joeurs 8
+#define NB_max_joueurs 8
 
 typedef struct joueur{
     char nom[DIM_STR];
@@ -11,4 +11,4 @@ typedef struct joueur{
 
 int pile[DIM_pile];
 int defausse[DIM_pile];
-joueur joueurs[NB_max_joeurs];
+joueur joueurs[NB_max_joueurs];
