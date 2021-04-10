82ba1df9320fa2e985570bb3a503c386597a8bd7
RedBlaze42 <allairehugo@gmail.com>
2021-04-10 13:55:08 +0200

    Ajout de la fonction afficher joueurs

--- affichage.c
+++ affichage.c
@@ -10,3 +10,25 @@ void banniere()
         printf("%c",205);
     printf("%c\n\n",188);
 }
+
+void afficher_joueurs(S_joueur joueurs[NB_max_joueurs], int nb_joueurs, int index_donneur)
+{
+    for(int i=0; i<nb_joueurs; i++)
+    {
+        printf("%-10s",joueurs[i].nom);
+        index_donneur==i ? printf("(D) ") : printf("( ) ");
+        for(int j=0; j<DIM_main_joeur; j++)
+        {
+            if(joueurs[i].cartes[j]==CARTE_NULLE)
+            {
+                printf("%c ",176);
+            }else{
+                printf("%c ",219);
+            }
+        }
+
+        for(int j=0;j<joueurs[i].nb_jetons;j++)
+            printf("%c ",254);
+        printf("\n\n");
+    }
+}
--- affichage.h
+++ affichage.h
@@ -3,7 +3,9 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include "declarations.h"
 
 void banniere();
+void afficher_joueurs(S_joueur joueurs[NB_max_joueurs], int nb_joueurs, int index_donneur);
 
 #endif
