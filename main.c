f4f2386bd7740800282f096aa4eb1f78325a30d0
RedBlaze42 <allairehugo@gmail.com>
2021-04-10 15:38:43 +0200

    Changement mineurs main

--- main.c
+++ main.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include "util.h"
 #include "init.h"
 #include "gestion_manche.h"
 #include "affichage.h"
 #include "declarations.h"
-#include <time.h>
+#include "debug.h"
 
 int main()
 {
     srand(time(NULL));
     index_donneur=0;
-    initialiser_joueurs(joueurs, &nb_joueurs);
+    /*initialiser_joueurs(joueurs, &nb_joueurs);
     system("cls");
-    afficher_joueurs(joueurs, nb_joueurs, index_donneur);
+    afficher_joueurs(joueurs, nb_joueurs, index_donneur);*/
+    initialiser_pile(pile);
+    afficher_pile(pile);
     return 0;
 }
