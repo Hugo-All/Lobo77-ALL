7ae1b0b38df88bc56d40fb571ce4ff77395326e6
RedBlaze42 <allairehugo@gmail.com>
2021-04-10 12:09:26 +0200

    Fonction gets_limite

--- util.c
+++ util.c
@@ -5,3 +5,9 @@ void color (int couleurDuTexte, int couleurDuFond)
      HANDLE H = GetStdHandle(STD_OUTPUT_HANDLE);
      SetConsoleTextAttribute(H, couleurDuFond*16+couleurDuTexte);
 }
+
+void gets_limite(int limite, char *sortie)
+{
+     fgets(sortie,limite, stdin);
+     fflush(stdin);
+}
