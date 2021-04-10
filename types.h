9c41421330abffba707ddcb407c4b12a2a3d757d
RedBlaze42 <allairehugo@gmail.com>
2021-04-10 11:30:47 +0200

    Ajout des types de base

--- types.h
+++ types.h
@@ -1,1 +1,14 @@
+#define DIM_STR 10
+#define DIM_pile 56
+#define DIM_main_joeur 5
+#define NB_max_joeurs 8
 
+typedef struct joueur{
+    char nom[DIM_STR];
+    int cartes[5];
+    int nb_jetons;
+} joueur;
+
+int pile[DIM_pile];
+int defausse[DIM_pile];
+joueur joueurs[NB_max_joeurs];
