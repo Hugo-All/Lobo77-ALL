c9190b5ce2a831b3bafd3f3498fb0259bcf37fdf
RedBlaze42 <allairehugo@gmail.com>
2021-04-10 15:38:09 +0200

    Ajout de la fonction debug afficher_pile

--- debug.c
+++ debug.c
@@ -1,1 +1,16 @@
 #include "debug.h"
+
+
+void tableau_cartes(int pile[DIM_pile])
+{
+
+}
+
+void afficher_pile(int pile[DIM_pile])
+{
+    for(int i=0; i<DIM_pile;i++)
+    {
+        printf("%d ", pile[i]);
+    }
+    printf("\n");
+}
--- debug.h
+++ debug.h
@@ -1,5 +1,10 @@
 #ifndef INCLUDE_DEBUG_H
 #define INCLUDE_DEBUG_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include "declarations.h"
+
+void afficher_pile(int pile[DIM_pile]);
 
 #endif
