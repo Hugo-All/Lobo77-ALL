e85713c8cf44d39a4a8576e9a94c6098da97277c
RedBlaze42 <allairehugo@gmail.com>
2021-04-10 17:16:47 +0200

    Fonction distribuer cartes

--- init.c
+++ init.c
@@ -54,3 +54,26 @@ void initialiser_defausse(int defausse[DIM_pile])
     for(int i = 0; i < DIM_pile; i++)
         defausse[i] = CARTE_NULLE;
 }
+
+void distribuer_cartes(int pile[DIM_pile], int index_pile, S_joueur joueurs[NB_max_joueurs], int nb_joueurs, int index_donneur)
+{
+    system("cls");
+    printf("Distribution des cartes...\n");
+    sleep(2);
+    for(int i = 0; i < DIM_main_joueur; i++)
+    {
+        for(int j = 0; j < nb_joueurs; j++)
+        {
+            system("cls");
+            afficher_joueurs(joueurs, nb_joueurs, index_donneur);
+            usleep(300000);
+            joueurs[j].cartes[i] = pile[index_pile];
+            pile[index_pile] = CARTE_NULLE;
+            index_pile--;
+        }
+    }
+    system("cls");
+    afficher_joueurs(joueurs, nb_joueurs, index_donneur);
+    printf("Fin de la distribution des cartes\n");
+    sleep(2);
+}
--- init.h
+++ init.h
@@ -3,12 +3,17 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+
 #include "util.h"
 #include "declarations.h"
+#include "affichage.h"
 
 void initialiser_joueurs(S_joueur joueurs[NB_max_joueurs], int *nb_joueurs);
 
 void initialiser_pile(int pile[DIM_pile]);
 void initialiser_defausse(int defausse[DIM_pile]);
+
+void distribuer_cartes(int pile[DIM_pile], int index_pile, S_joueur joueurs[NB_max_joueurs], int nb_joueurs, int index_donneur);
 
 #endif
