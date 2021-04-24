035784905ae35dca9681669342e2e8aff5bce71a
RedBlaze42 <allairehugo@gmail.com>
2021-04-24 16:49:54 +0200

    Fonction afficher cartes

--- affichage.c
+++ affichage.c
@@ -41,3 +41,89 @@ void afficher_joueurs(S_joueur joueurs[NB_max_joueurs], int nb_joueurs, int inde
         printf("\n\n");
     }
 }
+
+void afficher_cartes(S_joueur joueur, int curseur) //TODO Refactor avec print line ?
+{
+    for(int i_carte = 0; i_carte < DIM_main_joueur; i_carte++)
+    {
+        color(BLANC, NOIR);
+        if(joueur.cartes[i_carte] != CARTE_NULLE)
+        {
+            if(i_carte == curseur) color(NOIR, BLANC);
+            printf("%c%c%c%c%c%c%c%c", 201, 205, 205, 205, 205, 205, 205, 187);
+        }
+        else
+            printf("        ");
+    }
+    printf("\n");
+
+    for(int i_ligne = 0; i_ligne < 2; i_ligne++)
+    {
+        for(int i_carte = 0; i_carte < DIM_main_joueur; i_carte++)
+        {
+            color(BLANC, NOIR);
+            if(joueur.cartes[i_carte] != CARTE_NULLE)
+            {
+                if(i_carte == curseur) color(NOIR, BLANC);
+                printf("%c      %c", 186, 186);
+            }
+            else
+                printf("        ");
+        }
+        printf("\n");
+    }
+    
+    for(int i_carte = 0; i_carte < DIM_main_joueur; i_carte++)
+    {
+        color(BLANC, NOIR);
+        if(joueur.cartes[i_carte] != CARTE_NULLE)
+        {
+            if(i_carte == curseur) color(NOIR, BLANC);
+            switch (joueur.cartes[i_carte])
+            {
+                case CARTE_SENS :
+                    printf("%c SENS %c", 186, 186);
+                    break;
+                case CARTE_X2 :
+                    printf("%c  X2  %c", 186, 186);
+                    break;
+                default :
+                    printf("%c % 3d  %c", 186, joueur.cartes[i_carte], 186);
+            }
+        }   
+        else
+            printf("        ");
+    }
+    printf("\n");
+    
+    for(int i_ligne = 0; i_ligne < 2; i_ligne++)
+    {
+        for(int i_carte = 0; i_carte < DIM_main_joueur; i_carte++)
+        {
+            color(BLANC, NOIR);
+            if(joueur.cartes[i_carte] != CARTE_NULLE)
+            {
+                if(i_carte == curseur) color(NOIR, BLANC);
+                printf("%c      %c", 186, 186);
+            }
+            else
+                printf("        ");
+        }
+        printf("\n");
+    }
+    
+    for(int i_carte = 0; i_carte < DIM_main_joueur; i_carte++)
+    {
+        color(BLANC, NOIR);
+        if(joueur.cartes[i_carte] != CARTE_NULLE)
+        {
+            if(i_carte == curseur) color(NOIR, BLANC);
+            printf("%c%c%c%c%c%c%c%c", 200, 205, 205, 205, 205, 205, 205, 188);
+        }
+        else
+            printf("        ");
+    }
+    printf("\n");
+    color(BLANC, NOIR);
+    
+}
--- affichage.h
+++ affichage.h
@@ -4,8 +4,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "declarations.h"
+#include "util.h"
 
 void banniere();
 void afficher_joueurs(S_joueur joueurs[NB_max_joueurs], int nb_joueurs, int index_donneur);
+void afficher_cartes(S_joueur joueur, int curseur);
 
 #endif
