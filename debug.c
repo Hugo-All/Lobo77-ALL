5d32663a25dff80cb17c4d73683125ee051db925
RedBlaze42 <allairehugo@gmail.com>
2021-04-24 16:45:52 +0200

    Fonction distribuer cartes DEBUG

--- debug.c
+++ debug.c
@@ -29,3 +29,27 @@ void initialiser_joueurs_DEBUG(S_joueur joueurs[NB_max_joueurs], int *nb_joueurs
         joueurs[i].nb_jetons = 3;
     }
 }
+
+void distribuer_cartes_DEBUG(int pile[DIM_pile], int* index_pile, S_joueur joueurs[NB_max_joueurs], int nb_joueurs, int index_donneur)//distribution des cartes
+{
+    //system("cls");
+    //printf("Distribution des cartes...\n");
+    //sleep(2);
+    for(int i = 0; i < DIM_main_joueur; i++)
+    {
+        for(int j = 0; j < nb_joueurs; j++)
+        {
+            //system("cls");
+            //afficher_joueurs(joueurs, nb_joueurs, index_donneur);
+            //usleep(300000);
+            joueurs[j].cartes[i] = pile[*index_pile];
+            pile[*index_pile] = CARTE_NULLE;
+            (*index_pile)--;
+        }
+    }
+    //system("cls");
+    //afficher_joueurs(joueurs, nb_joueurs, index_donneur);
+    //printf("Fin de la distribution des cartes\n");
+    //sleep(2);
+    //system("cls");
+}
--- debug.h
+++ debug.h
@@ -7,4 +7,5 @@
 
 void afficher_pile(int pile[DIM_pile]);
 void initialiser_joueurs_DEBUG(S_joueur joueurs[NB_max_joueurs], int *nb_joueurs);
+void distribuer_cartes_DEBUG(int pile[DIM_pile], int* index_pile, S_joueur joueurs[NB_max_joueurs], int nb_joueurs, int index_donneur);
 #endif
