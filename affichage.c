d81a51eeb35216a256046bd7f74e228ca884d746
RedBlaze42 <allairehugo@gmail.com>
2021-04-24 16:41:32 +0200

    Amélioration fonction afficher joueurs

--- affichage.c
+++ affichage.c
@@ -3,25 +3,27 @@
 void banniere()
 {
     printf("%c", 200);
-    for(int i = 0; i < 20; i++)
+    for(int i = 0; i < 16; i++)
         printf("%c", 205);
     printf("LOBO 77");
-    for(int i = 0; i < 20; i++)
+    for(int i = 0; i < 16; i++)
         printf("%c", 205);
     printf("%c\n\n", 188);
 }
 
 void afficher_joueurs(S_joueur joueurs[NB_max_joueurs], int nb_joueurs, int index_donneur)
 {
+    banniere();
+    printf("Nom      Donneur  Cartes       Jetons\n");
     for(int i = 0; i < nb_joueurs; i++)
     {
         printf("%-10s", joueurs[i].nom);
         if(index_donneur == i)
         {
-            printf(" (D) ");
+            printf(" (D)    ");
         }else
         {
-            printf(" ( ) ");
+            printf(" ( )    ");
         }
 
         for(int j = 0; j < DIM_main_joueur; j++)
@@ -33,7 +35,7 @@ void afficher_joueurs(S_joueur joueurs[NB_max_joueurs], int nb_joueurs, int inde
                 printf("%c ", 219);
             }
         }
-
+        printf("   ");
         for(int j = 0; j < joueurs[i].nb_jetons; j++)
             printf("%c ", 254);
         printf("\n\n");
