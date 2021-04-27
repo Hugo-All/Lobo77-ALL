5ad4314c02b4f88f8690c95a2f0b4ac799b7e0c4
RedBlaze42 <allairehugo@gmail.com>
2021-04-27 22:25:23 +0200

    Fonction vider_defausse

--- gestion_manche.c
+++ gestion_manche.c
@@ -1,1 +1,11 @@
-#include "gestion_manche.h"
+#include "gestion_manche.h"void vider_defausse(int pile[DIM_pile], int defausse[DIM_pile], int *index_pile, int *index_defausse)
+{
+    for(int i = *index_defausse; i >= 0; i--)
+    {
+        (*index_pile)++; //Incrémente le nombre de carte dans la pile
+        (*index_defausse)--; //Décrémente le nombre de carte dans la défausse
+
+        pile[*index_pile] = defausse[i]; //Transfère la carte de la défausse vers la pile
+        defausse[i] = CARTE_VIDE; //Remplace la carte transférée par la carte vide
+    }
+}
--- gestion_manche.h
+++ gestion_manche.h
@@ -3,4 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+
+void vider_defausse(int pile[DIM_pile], int defausse[DIM_pile], int *index_pile, int *index_defausse);
+
 #endif
