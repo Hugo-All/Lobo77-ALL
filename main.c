2937934135c9570f986eb5fdcd2bac2bb709b9be
RedBlaze42 <allairehugo@gmail.com>
2021-04-28 18:22:25 +0200

    Donneur avant la distribution
    Pas oublier de distribuer à partir du donneur

--- main.c
+++ main.c
@@ -23,9 +23,9 @@ int main()
     printf("Bienvenue sur LOBO77. Par Hugo ALLAIRE et Tom ALLARD\n\n");
 
     initialiser_joueurs(joueurs, &nb_joueurs);
+    donneur_aleatoire(joueurs, nb_joueurs);
     distribuer_jetons(joueurs, nb_joueurs);
     distribuer_cartes(pile, &index_pile, joueurs, nb_joueurs);
-    donneur_aleatoire(joueurs, nb_joueurs);
     
     manche(joueurs, nb_joueurs, pile, &index_pile);
     system("cls");
