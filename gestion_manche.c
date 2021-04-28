b8005f000b8abc70f3b1d50c65f5652776d215eb
RedBlaze42 <allairehugo@gmail.com>
2021-04-28 15:07:22 +0200

    Support carte x2
    Problème:
    Redite de "laisser le clavier à "
    Si le joueur n'as plus de cartes, il est "protégé" ?

--- gestion_manche.c
+++ gestion_manche.c
@@ -9,7 +9,8 @@ void manche(S_joueur joueurs[NB_max_joueurs], int nb_joueurs, int pile[DIM_pile]
     int index_defausse = 0; //Nombre de cartes dans la défausse
     int total_defausse = 0; //Total numérique de la défausse
     int index_carte, valeur_carte; //Stoke la carte sélectionnée par le joueur et sa valeur
-    int prochain_joueur_x2 = 0; //Indique si le prochain joueur doit jouer 2 fois
+    int joueur_suivant_x2 = 0; //Indique si le prochain joueur doit jouer 2 fois
+    int carte_restant_a_jouer = 1; //Indique le nombre de cartes à jouer par le joueur
 
     for(int i = 0; i < DIM_pile; i++)
         defausse[i] = CARTE_VIDE; //Initialise la défausse avec des cartes vides
@@ -38,79 +39,91 @@ void manche(S_joueur joueurs[NB_max_joueurs], int nb_joueurs, int pile[DIM_pile]
 
     while(total_defausse < 77 && nb_joueur_valide(joueurs, nb_joueurs) > 1)//TODO Carte x2
     {
+        if(joueur_suivant_x2 == 0) //Si le joueur précédent a joué une carte x2
+            carte_restant_a_jouer = 1;
+        else
+            carte_restant_a_jouer = 2;
+        joueur_suivant_x2 = 0;
 
-        if(*index_pile == 0) //Si il ne reste plus de cartes dans la pile
-        {
-            vider_defausse(pile, defausse, index_pile, &index_defausse);
-            afficher_joueurs_et_total(joueurs, nb_joueurs, total_defausse);
-            printf("La défausse a ete rajoutee a la pile et melangee.\n");
-        }
-
-        afficher_joueurs_et_total(joueurs, nb_joueurs, total_defausse);
-        printf("Laissez le clavier a %s\n", joueurs[index_joueur].nom);
-        system("pause");
-        index_carte = selectionner_carte(joueurs, joueurs[index_joueur], nb_joueurs, total_defausse);
-        valeur_carte = joueurs[index_joueur].cartes[index_carte];
-
-        //Le joueur se défausse de la carte qu'il vient de jouer
-        defausse[index_defausse + 1] = valeur_carte;
-        index_defausse++;
-        defausse[index_defausse] = valeur_carte;
-        joueurs[index_joueur].cartes[index_carte] = CARTE_VIDE;
-
-
-        if(valeur_carte == CARTE_SENS)//Inversion du sens de jeu
-        {
-            if(joueurs[index_joueur].sens_jeu == SENS_ANTIHORAIRE)
-                joueurs[index_joueur].sens_jeu = SENS_HORAIRE;
-            else
-                joueurs[index_joueur].sens_jeu = SENS_ANTIHORAIRE;
-
-            afficher_joueurs_et_total(joueurs, nb_joueurs, total_defausse);
-            printf("Le sens de jeu a ete inverse.\n");
-        }else if(valeur_carte == CARTE_X2)
-        {
-            afficher_joueurs_et_total(joueurs, nb_joueurs, total_defausse);
-            prochain_joueur_x2 = 1;
-            printf("Le prochain joueur devra jouer 2 cartes\n");
-        }else //La carte a donc une valeur numérique
-        {
-            total_defausse += valeur_carte;
-            afficher_joueurs_et_total(joueurs, nb_joueurs, total_defausse);
-            printf("La carte %d a ete ajoutee au total qui atteint donc: %d\n", valeur_carte, total_defausse);
-            if(total_defausse != 0 && total_defausse < 77 && total_defausse%11 == 0) //Si le total atteint un multiple de 11, le joueur perd un jeton.
+        do{
+            if(*index_pile == 0) //Si il ne reste plus de cartes dans la pile
             {
-                printf("\nLe total est un multiple de 11. ");
-                retirer_jeton(&joueurs[index_joueur]);
-            }
-        }
-
-        //Si le joueur est toujours vivant et que le total est inférieur à 77
-        if(joueurs[index_joueur].nb_jetons >= 0 && total_defausse < 77)
-        {
-            printf("Vous avez 5 secondes pour piocher une carte.\n");
-            if(attend_touche(5) == 1)
-            {
-                joueurs[index_joueur].cartes[index_carte] = pile[*index_pile];
-                pile[*index_pile] = CARTE_VIDE;
-                (*index_pile)--;
-
+                vider_defausse(pile, defausse, index_pile, &index_defausse);
                 afficher_joueurs_et_total(joueurs, nb_joueurs, total_defausse);
-                printf("Vous avez pioche la carte:\n\n");
-                afficher_carte(joueurs[index_joueur].cartes[index_carte]);
-
-            }else
-            {
-                afficher_joueurs_et_total(joueurs, nb_joueurs, total_defausse);
-                printf("Vous n'avez pas pioche de carte.\n");
-                if(nb_cartes_joueur(joueurs[index_joueur]) == 0)
-                    printf("Vous n'avez plus de cartes, vous ne perdez pas de jetons, mais vous devez attendre la prochaine manche.\n");
+                printf("La défausse a ete rajoutee a la pile et melangee.\n");
             }
+
+            afficher_joueurs_et_total(joueurs, nb_joueurs, total_defausse);
+            printf("Laissez le clavier a %s, qui doit jouer %d carte(s)\n", joueurs[index_joueur].nom, carte_restant_a_jouer);
+            printf("\n%d\n", carte_restant_a_jouer);
             system("pause");
-        }
+
+            do{
+                index_carte = selectionner_carte(joueurs, joueurs[index_joueur], nb_joueurs, total_defausse);
+                valeur_carte = joueurs[index_joueur].cartes[index_carte];
+            }while(valeur_carte == CARTE_X2 && carte_restant_a_jouer == 2); //Si le joueur doit jouer 2 cartes, il ne peut pas jouer un x2 en première carte
+
+            //Le joueur se défausse de la carte qu'il vient de jouer
+            defausse[index_defausse + 1] = valeur_carte;
+            index_defausse++;
+            defausse[index_defausse] = valeur_carte;
+            joueurs[index_joueur].cartes[index_carte] = CARTE_VIDE;
+
+            //Traitement de la carte jouée
+            if(valeur_carte == CARTE_SENS) //Inversion du sens de jeu
+            {
+                if(joueurs[index_joueur].sens_jeu == SENS_ANTIHORAIRE)
+                    joueurs[index_joueur].sens_jeu = SENS_HORAIRE;
+                else
+                    joueurs[index_joueur].sens_jeu = SENS_ANTIHORAIRE;
+
+                afficher_joueurs_et_total(joueurs, nb_joueurs, total_defausse);
+                printf("Le sens de jeu a ete inverse.\n");
+                carte_restant_a_jouer--;
+            }else if(valeur_carte == CARTE_X2) //Le prochain joueur doit jouer
+            {
+                afficher_joueurs_et_total(joueurs, nb_joueurs, total_defausse);
+                joueur_suivant_x2 = 1;
+                printf("Le prochain joueur devra jouer 2 cartes\n");
+                carte_restant_a_jouer--;
+            }else //La carte a donc une valeur numérique
+            {
+                total_defausse += valeur_carte;
+                afficher_joueurs_et_total(joueurs, nb_joueurs, total_defausse);
+                printf("La carte %d a ete ajoutee au total qui atteint donc: %d\n", valeur_carte, total_defausse);
+                if(total_defausse != 0 && total_defausse < 77 && total_defausse%11 == 0) //Si le total atteint un multiple de 11, le joueur perd un jeton.
+                {
+                    printf("\nLe total est un multiple de 11. ");
+                    retirer_jeton(&joueurs[index_joueur]);
+                }
+                carte_restant_a_jouer--;
+            }
+
+            //Si le joueur est toujours vivant et que le total est inférieur à 77
+            if(joueurs[index_joueur].nb_jetons >= 0 && total_defausse < 77)
+            {
+                printf("Vous avez 5 secondes pour piocher une carte.\n");
+                if(attend_touche(5) == 1)
+                {
+                    joueurs[index_joueur].cartes[index_carte] = pile[*index_pile];
+                    pile[*index_pile] = CARTE_VIDE;
+                    (*index_pile)--;
+
+                    afficher_joueurs_et_total(joueurs, nb_joueurs, total_defausse);
+                    printf("Vous avez pioche la carte:\n\n");
+                    afficher_carte(joueurs[index_joueur].cartes[index_carte]);
+                }else
+                {
+                    afficher_joueurs_et_total(joueurs, nb_joueurs, total_defausse);
+                    printf("Vous n'avez pas pioche de carte.\n");
+                    if(nb_cartes_joueur(joueurs[index_joueur]) == 0)
+                        printf("Vous n'avez plus de cartes, vous ne perdez pas de jetons, mais vous devez attendre la prochaine manche.\n");
+                }
+                system("pause");
+            }
+        }while(carte_restant_a_jouer > 0 && joueurs[index_joueur].nb_jetons >= 0 && nb_cartes_joueur(joueurs[index_joueur]) > 0);
 
         index_joueur = joueur_suivant(joueurs, nb_joueurs);
-
     }
 
 }
