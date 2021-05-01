d6402b3979bc2753f5b71f7a93c18676546f9006
RedBlaze42 <allairehugo@gmail.com>
2021-05-01 19:37:21 +0200

    Fix #4

--- affichage.c
+++ affichage.c
@@ -38,7 +38,7 @@ void afficher_joueurs(S_joueur joueurs[NB_max_joueurs], int nb_joueurs)
 
         printf("%-10s ", joueurs[i].nom); //Affiche le nom du joueur
 
-        //Si un jeton n'as plus de jetons et n'est pas flottant, on ne l'affiche pas
+        //Il faut qu'un joueur soit toujours vivant pour être affiché
         if(joueurs[i].nb_jetons >= 0)
         {
             //Affiche les jetons du joueur
