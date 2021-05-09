ba14f5142d185ba87f87ce082cfea4fada1819bf
RedBlaze42 <allairehugo@gmail.com>
2021-05-09 17:43:09 +0200

    Remplace "pile" par "pioche" (à verifier)

--- gestion_manche.h
+++ gestion_manche.h
@@ -11,14 +11,14 @@
 #include "init.h"
 
 // Programme général de gestion d'une manche
-void manche(S_joueur joueurs[NB_max_joueurs], int nb_joueursw, int pile[DIM_pile], int *index_pile);
+void manche(S_joueur joueurs[NB_max_joueurs], int nb_joueursw, int pioche[DIM_pile], int *index_pioche);
 
 // Propose à un joueur de piocher un nombre "cartes_a_piocher" de cartes
-void piocher(S_joueur joueurs[NB_max_joueurs], int nb_joueurs, S_joueur *joueur, int pile[DIM_pile], int *index_pile, int cartes_a_piocher, int total_defausse);
+void piocher(S_joueur joueurs[NB_max_joueurs], int nb_joueurs, S_joueur *joueur, int pioche[DIM_pile], int *index_pioche, int cartes_a_piocher, int total_defausse);
 
 // Vide la défausse, la place au dessus de la pioche puis mélange la pioche
-void vider_defausse(int pile[DIM_pile], int defausse[DIM_pile], int *index_pile, int *index_defausse);
-// Vide la main des joueurs dans la pile. Avec une animation
-void vider_main_joueurs(S_joueur joueurs[NB_max_joueurs], int nb_joueurs, int pile[DIM_pile], int *index_pile);
+void vider_defausse(int pioche[DIM_pile], int defausse[DIM_pile], int *index_pioche, int *index_defausse);
+// Vide la main des joueurs dans la pioche. Avec une animation
+void vider_main_joueurs(S_joueur joueurs[NB_max_joueurs], int nb_joueurs, int pioche[DIM_pile], int *index_pioche);
 
 #endif
