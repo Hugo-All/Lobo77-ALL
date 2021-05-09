b9255dc070ec200afd6c8a291c671a1d0ea3746f
RedBlaze42 <allairehugo@gmail.com>
2021-05-09 14:05:11 +0200

    Commentaires init.h

--- init.h
+++ init.h
@@ -9,13 +9,18 @@
 #include "declarations.h"
 #include "affichage.h"
 
+// Demande à l'utilisateur le nombre de joueurs et leurs noms
 void initialiser_joueurs(S_joueur joueurs[NB_max_joueurs], int *nb_joueurs);
 
+// Place dans "pile" les cartes du jeu
 void initialiser_pile(int pile[DIM_pile]);
 
+// Sélectionne un joueur vivant aléatoire et le nomme donneur. Avec une animation
 void donneur_aleatoire(S_joueur joueurs[NB_max_joueurs], int nb_joueurs);
 
-void distribuer_cartes(int pile[DIM_pile], int* index_pile, S_joueur joueurs[NB_max_joueurs], int nb_joueurs);
+// Distribue aux joueurs vivants 5 cartes à partir de la pioche. Avec une animation
+void distribuer_cartes(int poche[DIM_pile], int* index_pioche, S_joueur joueurs[NB_max_joueurs], int nb_joueurs);
+// Distribue au joueurs un nombre "jetons_depart" (variable globale) de jetons. Avec une animation
 void distribuer_jetons(S_joueur joueurs[NB_max_joueurs], int nb_joueurs);
 
 #endif
